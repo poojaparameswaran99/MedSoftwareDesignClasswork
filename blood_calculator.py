a464faa546a3676d0c361de4ac4cf50d9f051ca5
pkp14 <pkp14@duke.edu>
2023-01-20 12:57:08 -0500

    added HDL analysis function to analyze HDL value qualitatively (low, high)

---
+++ blood_calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 12:28:59 2023
+
+@author: poojap
+"""
+
+def interface():
+    print("Blood calculator")
+    keep_running = True
+    while keep_running:
+        print("Options:")
+        print("1 - HDL")
+        print("9 - Quit")
+        choice = input("Select an option:")
+        if choice == "9":
+            keep_running = False
+        elif choice == "1":
+            HDL_driver()
+        print("Program ending")
+        
+
+
+def HDL_analysis(HDL_int):
+    if HDL_int >= 60:
+        answer = "Normal"
+    elif 40<= HDL_int < 60:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
+
+
+
+interface()
