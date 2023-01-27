927ea9216438e45338e6c5d53a2fc4db15614a43
pkp14 <pkp14@duke.edu>
2023-01-27 12:14:59 -0500

    adding to fix_outptu branch with specifications on rounding

--- output_readability.py
+++ output_readability.py
@@ -3,6 +3,6 @@ dosage = weight * 30
 
 print("CORRECT DOSAGE")
 print("For a patient weighing {:.1f} kg,".format(weight))
-print("  the correct dosage is {:.1f} mg the first day".format(dosage))
+print("  the correct dosage is {:.1f } mg the first day".format(dosage))
 
 
