43b1eef936ab0cbc3fbb0067d416ca6918354367
pkp14 <pkp14@duke.edu>
2023-02-08 12:42:39 -0500

    updated with pytest.approx instead of round

--- test_coordinatePlan.py
+++ test_coordinatePlan.py
@@ -21,8 +21,8 @@ def test_checkNewX(coor1, coor2,x3, expected):
     
     
 @pytest.mark.parametrize("coor1, coor2, expected",
-                         [((-1,5), (3,7), (round(1/2,2), round(11/2, 2))),
-                          ((1,5), (4,10), (round(5/3, 2), round(10/3,2))),
+                         [((-1,5), (3,7), (1/2,2), (11/2))),
+                          ((1,5), (4,10), (5/3, 2), (10/3,2))),
                           ((10,10), (5,5), (1, 0))])
 def test_findLine(coor1, coor2, expected):
     from coordinatePlan import findLine
@@ -31,20 +31,20 @@ def test_findLine(coor1, coor2, expected):
     answer = findLine(coor1, coor2)
     
     #assert 
-    assert answer == expected
+    assert answer == pytest.approx(expected)
     
     
 @pytest.mark.parametrize("slope, yInt, x3, expected",
-                         [(round(1/2,2), round(11/2,2), 3, 7),
-                          (round(5/3, 2), round(10/3,2), 3, 8.34)]
+                         [((1/2,2), (11/2, 3), 7),
+                          ((5/3, 2), (10/3, 3) , 8.34)]
                          )
 def test_findYcoordinate(slope, yInt, x3, expected):
     from coordinatePlan import findYcoordinate
-    
+
     #Act
     answer = findYcoordinate(slope, yInt, x3)
     
     # Assert
-    assert answer == expected
+    assert answer ==pytest.approx(expected)
     
     
