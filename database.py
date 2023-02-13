9362152d09fc0161c9ded502697f3e3b028d4607
pkp14 <pkp14@duke.edu>
2023-02-13 10:23:59 -0500

    updated main_driver to call appropriate functions and finished get_test_result

--- database.py
+++ database.py
@@ -36,18 +36,20 @@ def main_driver():
     db = {}
     db [1] = (create_patient_entry("Ann Ables", 1, 34))
     db [2] = (create_patient_entry("Bob Boyles", 2, 45))
-    db[3] = (create_patient_entry("Chris Chou", 3, 52))
+    db [3] = (create_patient_entry("Chris Chou", 3, 52))
     print_database(db)
     
     add_test_to_patient(db, 1, "HDL", 120)
     add_test_to_patient(db, 2, "LDL", 100)
-    room_numbers = ["103", "232", "333"]
-    
+    add_test_to_patient(db, 3, "HDL", 99)
     print(db)
-    print_directory(db, room_numbers)
-    testval = find_Test_val(db, 2, "LDL" )
-    print(testval)
-
+    print_database(db)
+    # room_numbers = ["103", "232", "333"]
+    
+    # print_directory(db, room_numbers)
+    gettestresult = get_test_result(db, 2, "LDL")
+    print(gettestresult)
+    return
 
 def find_Test_val(db, mrn_to_find, testname):
     for patient in db:
@@ -66,7 +68,7 @@ def print_directory(db, room_numbers):
 
 
 def get_patient_entry(db, mrn_to_find):
-    patient = db.get([mrn_to_find])
+    patient = db.get(mrn_to_find)
     if patient is None:
         return False
     return patient
@@ -86,6 +88,11 @@ def get_test_value_from_test_list(test_list, test_name):
         if test[0] == test_name:
             return test[1]
     return False
+
+def get_test_result(db, mrn, test_name):
+    patient = get_patient_entry(db, mrn)
+    test_value = get_test_value_from_test_list(patient["Tests"], test_name)
+    return test_value
 
 
 if __name__ == "__main__":
