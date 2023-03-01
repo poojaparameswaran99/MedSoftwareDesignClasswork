685a2f77a4f04f7f4752fe48006e510adabae6bc
pkp14 <pkp14@duke.edu>
2023-03-01 12:57:04 -0500

    finished class assignment to compare blood donors and recipeients, post answer and see if correct

---
+++ blood_match.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 12:45:13 2023
+
+@author: poojap
+"""
+
+import requests
+
+r_get = requests.get("http://vcm-7631.vm.duke.edu:5002/get_patients/pkp14")
+print(r_get.text)
+
+rgetDonor = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/M8")
+
+rgetRecipient = requests.get("http://vcm-7631.vm.duke.edu:5002/get_blood_type/M5")
+
+print(rgetDonor.text)
+print(rgetRecipient.text)
+# NO! B+ cannot recieve from AB-
+
+out_data = {"Name": "pkp14", "Match": "No"}
+rpost = requests.post("http://vcm-7631.vm.duke.edu:5002/match_check",
+                  json = out_data)
+
+print(rpost.text)
